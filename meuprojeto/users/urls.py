ef4b90139f4a808aa1c78440878d17638eab351b
wallas <wagratom@gmail.com>
2024-05-26 14:16:58 -0300

    feat: add route to get your own profile

--- meuprojeto/users/urls.py
+++ meuprojeto/users/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('all-friends/<user_id>', views.all_friends, name='all_friends'),
+    path('<user_id>', views.get_user, name='get_user'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
+    path('friends/<user_id>', views.user_friends, name='user_friends'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
+	# path('games/<user_id>', views.user_games, name='user_games'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
 ]
 
--- meuprojeto/users/views.py
+++ meuprojeto/users/views.py
@@ -3,16 +3,25 @@ from .models import User
 from django.http import JsonResponse
 # Create your views here.
 
-def index(request):
-	context = {
-		'users': User.objects.all()
+def get_user(request, user_id):
+	print(user_id)
+	user = User.objects.get(id=user_id)
+	response = {
+		'status': '200',
+		'id': user.id,
+		'name': user.name,
+		'nickname': user.nickname,
+		'avatar': user.avatar.name,
+		'online': user.online
 	}
-	return render(request, 'index.html', context=context)
+
+	return JsonResponse(response, status=200)
 
 
-def all_friends(request, user_id):
+def user_friends(request, user_id):
 	user = User.objects.get(id=user_id)
 	friends = user.friends.all().values(
+		'id',
 		'name',
 		'nickname',
 		'avatar',
@@ -20,5 +29,17 @@ def all_friends(request, user_id):
 	)
 
 	response = list(friends)
-	return JsonResponse({'status': '200', 'friends': response})
+	return JsonResponse({'status': '200', 'friends': response}, status=200)
 
+
+# def user_games(request, user_id):
+# 	user = User.objects.get(id=user_id)
+# 	games = user.games.all().values(
+# 		'id',
+# 		'winner',
+# 		'created_at',
+# 		'template'
+# 	)
+
+# 	response = list(games)
+# 	return JsonResponse({'status': '200', 'games': response}, status=200)
