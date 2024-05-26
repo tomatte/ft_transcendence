318e25a19ca0ef5e3cfd367eddec75e85e15fb94
wallas <wagratom@gmail.com>
2024-05-26 16:47:38 -0300

    feat: add route to get historicy

--- meuprojeto/users/urls.py
+++ meuprojeto/users/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<user_id>', views.get_user, name='get_user'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
-    path('friends/<user_id>', views.user_friends, name='user_friends'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
-	# path('games/<user_id>', views.user_games, name='user_games'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
+	path('<user_id>', views.get_user, name='get_user'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
+	path('friends/<user_id>', views.user_friends, name='user_friends'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
+	path('games/<user_id>', views.user_historicy, name='user_games'), ## TODO: por enquando recebendo o user, mas devemos salvar o user logado
 ]
 
--- meuprojeto/users/views.py
+++ meuprojeto/users/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from .models import User
+from .models import User, Game
 from django.http import JsonResponse
 # Create your views here.
+from django.db.models import Count
 
 def get_user(request, user_id):
-	print(user_id)
 	user = User.objects.get(id=user_id)
 	response = {
 		'status': '200',
@@ -32,14 +32,30 @@ def user_friends(request, user_id):
 	return JsonResponse({'status': '200', 'friends': response}, status=200)
 
 
-# def user_games(request, user_id):
-# 	user = User.objects.get(id=user_id)
-# 	games = user.games.all().values(
-# 		'id',
-# 		'winner',
-# 		'created_at',
-# 		'template'
-# 	)
+def user_historicy(request, user_id):
+	user = User.objects.get(id=user_id)
+	games = Game.objects.filter(players=user).prefetch_related(
+		'players',
+		'template',
+		'playeringame_set'
+	).annotate(matches=Count('playeringame'))
+	games_data = []
+	for game in games:
+		players_info = [
+			{
+				f'player{i}': player.name,
+				'winner': game.playeringame_set.get(user=player).winner
+			}
+			for  i, player in enumerate(game.players.all())
+		]
+		games_data.append({
+			'id': game.id,
+			'template': game.template.name,
+			'matches': game.matches,
+			'wiiners': game.playeringame_set.filter(user=user, winner=True).count(),
+			'loser': game.playeringame_set.filter(user=user, winner=False).count(),
+			'players': players_info,
+		})
 
-# 	response = list(games)
-# 	return JsonResponse({'status': '200', 'games': response}, status=200)
+	response = games_data
+	return JsonResponse({'status': '200', 'games': response}, status=200)
