309999db8b7288d9e726aadb430f3287d4063022
wallas <wagratom@gmail.com>
2024-05-26 13:55:56 -0300

    feat: add route to get  all friends

--- meuprojeto/users/urls.py
+++ meuprojeto/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('all-friends/<user_id>', views.all_friends, name='all_friends'),
 ]
 
--- meuprojeto/users/views.py
+++ meuprojeto/users/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 from .models import User
+from django.http import JsonResponse
 # Create your views here.
+
 def index(request):
 	context = {
 		'users': User.objects.all()
 	}
 	return render(request, 'index.html', context=context)
+
+
+def all_friends(request, user_id):
+	user = User.objects.get(id=user_id)
+	friends = user.friends.all().values(
+		'name',
+		'nickname',
+		'avatar',
+		'online'
+	)
+
+	response = list(friends)
+	return JsonResponse({'status': '200', 'friends': response})
+
