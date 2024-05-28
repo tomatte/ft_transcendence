b237334a37ebe63118ac16114bbc396bc7f4e1f5
wallas <wagratom@gmail.com>
2024-05-28 11:38:13 -0300

    feat: remodelando o banco de dados

--- meuprojeto/users/models.py
+++ meuprojeto/users/models.py
@@ -13,30 +13,46 @@ class User(models.Model):
 		return self.name
 
 
-class Game(models.Model):
+class Match(models.Model):
 	ROLE_CHOICES = (
 		('ranked', 'ranked'),
 		('normal', 'normal'),
 	)
 	id = models.AutoField(primary_key=True)
 	template = models.CharField(max_length=10, choices=ROLE_CHOICES)
-	players = models.ManyToManyField(User, blank=True, through='PlayerInGame')
 	create_at = models.DateTimeField(auto_now_add=True)
 	duration = models.DurationField(null=True, blank=True)
+	tournament = models.ForeignKey('Tournament', on_delete=models.CASCADE, null=True, blank=True, related_name='tournamentMath')
+	players = models.ManyToManyField(User, blank=True, through='MatchPlayer')
 
 
-class PlayerInGame(models.Model):
-	ROLE_CHOICES = (
-		('player1', 'player1'),
-		('player2', 'player2'),
-	)
-
+class MatchPlayer(models.Model):
 	id = models.AutoField(primary_key=True)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	game = models.ForeignKey(Game, on_delete=models.CASCADE)
-	role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+	player = models.ForeignKey(User, on_delete=models.CASCADE)
+	Match = models.ForeignKey(Match, on_delete=models.CASCADE)
 	winner = models.BooleanField(default=False)
-	points = models.IntegerField(default=0)
+	score 	= models.IntegerField(default=0)
 
 	def __str__(self):
 		return f"User: {self.user.name} GameID: {self.game.id}"
+
+
+class Tournament(models.Model):
+	id = models.AutoField(primary_key=True)
+	players = models.ManyToManyField(User, blank=True, through='PlayerInTournament')
+	match = models.ManyToManyField(Match, blank=True, related_name='matchTournament')
+
+	def __str__(self):
+		return self.name
+
+
+class PlayerInTournament(models.Model):
+	id = models.AutoField(primary_key=True)
+	player = models.ForeignKey(User, on_delete=models.CASCADE)
+	tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+	round_number = models.IntegerField(default=0)
+	score = models.IntegerField(default=0)
+
+	def __str__(self):
+		return f"User: {self.user.name} TournamentID: {self.tournament.id}"
+
