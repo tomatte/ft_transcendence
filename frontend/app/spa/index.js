ea61b99ef63fe21985edd1f71a3aab332dd59dc1
Emmanuel Tomiyoshi <tomiyoshiemmanuel@gmail.com>
2024-06-09 21:11:44 -0300

    added initial sidebar styles

--- frontend/app/spa/index.js
+++ frontend/app/spa/index.js
@@ -1,9 +1,18 @@
-function test() {
-    const body = document.querySelector('body');
+// function test() {
+//     const body = document.querySelector('body');
 
-    const p = document.createElement('p');
-    p.textContent = 'Hello from the SPA!';
-    body.appendChild(p);
-}
+//     const p = document.createElement('p');
+//     p.textContent = 'Hello from the SPA!';
+//     body.appendChild(p);
+// }
 
-test();
+// test();
+
+document.addEventListener("DOMContentLoaded", function() {
+	const expandCollapseButton = document.querySelector(".ExpandCollapseButton");
+	const sidebar = document.querySelector(".Sidebar");
+  
+	expandCollapseButton.addEventListener("click", function() {
+	  sidebar.classList.toggle("collapsed");
+	});
+  });
