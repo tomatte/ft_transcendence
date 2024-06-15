01f8b4e07ce96430b890d2dab9dc61d0fa653ffe
wallas <wagratom@gmail.com>
2024-06-15 20:26:30 -0300

    configure asgi

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,22 +1,26 @@
-"""
-ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
-"""
-
-import os
-
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from django.urls import path
+from websocket import views
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
+
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    # Just HTTP for now. (We can add other protocols later.)
+	"http": django_asgi_app,
+	"websocket": AllowedHostsOriginValidator(
+		AuthMiddlewareStack(
+			URLRouter([
+				# path("add_player_tournament/", views.Add_player_tournament.as_asgi()),
+				# path("remove_player_tournament/", views.Remove_player_tournament.as_asgi()),
+				# path("create_bracket/", views.Create_Bracket.as_asgi()),
+				# path("start_tournament/", views.Start_Tournament.as_asgi()),
+				# path("end_tournament/", views.End_Tournament.as_asgi()),
+			])
+		)
+	),
 })
