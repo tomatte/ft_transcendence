036793dace3fdc0eb317af9517597067e17a5174
caoslourenco <camillalourenco@outlook.com>
2024-06-19 14:02:03 -0300

    não funfa

--- frontend/app/teste_SPA/server.js
+++ frontend/app/teste_SPA/server.js
@@ -5,8 +5,19 @@ const app = express();
 
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 
+// app.get("/*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+// });
 app.get("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "index.html"));
+    res.sendFile(path.resolve(__dirname, "teste_SPA", "index.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
+--// app.get("/", (req, res) => {
+const port = process.env.PORT || 5060; // Definindo a porta padrão como 3000, caso não seja especificada via variável de ambiente
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+
+// app.listen(process.env.PORT || 3000, () => console.log("Server running..."));
