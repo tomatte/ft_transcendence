16eb22b4a1e7bcb13ddfb13be9665eb94d77196b
caoslourenco <camillalourenco@outlook.com>
2024-06-21 18:21:45 -0300

    localhost = home

--- frontend/app/src/scripts/index.js
+++ frontend/app/src/scripts/index.js
@@ -2,15 +2,23 @@ import routes from './router.js';
 
 const container = document.querySelector('#root');
 
-const init = () => window.addEventListener('hashchange', renderPage);
-const validateHash = (hash) => hash === ""  ? 'home' : hash.replace('#', '');
-
 const renderPage = () => {
-  const page = validateHash(window.location.hash);
-  container.innerHTML = '';
-  container.appendChild(routes[page]);
-}
-window.addEventListener('load', ()=> {
-  renderPage();
-  init();
+  const hash = window.location.hash.slice(1); 
+  const page = hash || 'Home'; 
+
+  if (routes[page]) {
+    container.innerHTML = '';
+    container.appendChild(routes[page]);
+  } else {
+    container.innerHTML = '<p>Página não encontrada</p>';
+  }
+};
+
+const init = () => {
+  window.addEventListener('hashchange', renderPage);
+};
+
+window.addEventListener('load', () => {
+  renderPage();  
+  init(); 
 });
