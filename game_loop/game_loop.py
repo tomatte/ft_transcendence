88b78148829737d08461e43f75151bbaa94d0e8e
Tomatte <daniel.brandao2000@gmail.com>
2024-06-24 16:07:36 -0300

    refact game_loop: create some classes

--- game_loop/game_loop.py
+++ game_loop/game_loop.py
@@ -3,11 +3,36 @@ import websockets
 import json
 from pong_entities import *
 
-class Game:
+class Socket:
     uri = "ws://localhost:8000/game_loop/"
+    ws: websockets.WebSocketClientProtocol = None
+    
+    @classmethod
+    async def  connect_to_server(cls):
+        cls.ws = await websockets.connect(cls.uri)
+        print("connected to server")
+
+    @classmethod
+    async def send_info(cls):
+        while True:
+            if len(Game.payload) > 0:
+                await cls.ws.send(json.dumps(Game.payload))
+            await asyncio.sleep(Game.fps_time)
+            
+    @classmethod
+    async def rcve_info(cls):
+        while True:
+            try:
+                msg = await Socket.ws.recv()
+                data = json.loads(msg)
+                print(f"data -> {data}")
+                Actions.act(data)
+            except:
+                pass
+
+class Game:
     balls: List[Ball] = []
     fps_time = 1 / FPS
-    websocket: websockets.WebSocketClientProtocol = None
     payload = {}
 
     @classmethod
@@ -19,62 +44,61 @@ class Game:
         cls.payload.clear()
         for ball in cls.balls:
             cls.payload[ball.id] = (ball.x, ball.y)
+            
+    @classmethod
+    def create_ball():
+        players = [
+            Player([0, 0], 900, 20, 100, Entity.PLAYER_LEFT),
+            Player([0, 0], 900, 20, 100, Entity.PLAYER_LEFT)
+        ]
+        ball = Ball([630, 350], 10, 500, 30, 1)
+        ball.set_players(players)
+        Game.balls.append(ball)
 
 
-def create_ball():
-    players = [
-        Player([0, 0], -1, 20, 100, Entity.PLAYER_LEFT),
-        Player([0, 0], -1, 20, 100, Entity.PLAYER_LEFT)
-    ]
-    ball = Ball([630, 350], 10, 500, 30, 1)
-    ball.set_players(players)
-    Game.balls.append(ball)
-
-async def  connect_to_server():
-    Game.websocket = await websockets.connect(Game.uri)
-    print("connected to server")
-
-async def send_info():
-    while True:
-        if len(Game.payload) > 0:
-            await Game.websocket.send(json.dumps(Game.payload))
-        await asyncio.sleep(Game.fps_time)
-        
-async def rcve_info():
-    while True:
-        try:
-            msg = await Game.websocket.recv()
-            data = json.loads(msg)
-            print(data)
-            if data["action"] == "new_game":
-                create_ball()
-            elif data["action"] == "move":
-                player_left = Player(
-                    [data["x"], data["y"]]
-                    , data["speed"], 
-                    data["width"], 
-                    data["height"], 
-                    Entity.PLAYER_LEFT
-                )
-                player_right = Player(
-                    [data["x"], data["y"]]
-                    , data["speed"], 
-                    data["width"], 
-                    data["height"], 
-                    Entity.PLAYER_RIGHT
-                )
-                Game.balls[0].set_players((player_left, player_right))
-        except:
-            pass
-        
-        
+class Actions:
+    @classmethod
+    def new_game(cls, data):
+        pass
+    
+    @classmethod
+    def new_player(cls, data):
+        pass
+    
+    @classmethod
+    def player_move(cls, data):
+        pass
+    
+    @classmethod
+    def player_disconnect(cls, data):
+        pass
+    
+    @classmethod
+    def act(cls, data):
+        if data["action"] == "new_game":
+            cls.new_game(data)
+            return 
+        if data["action"] == "new_player":
+            cls.new_player(data)
+            return 
+        if data["action"] == "player_move":
+            cls.player_move(data)
+            return 
+        if data["action"] == "player_disconnect":
+            cls.player_disconnect(data)
+            return     
 
 
 async def main():
-    await connect_to_server()
+    try:
+        await Socket.connect_to_server()
+    except:
+        print("failed to connect to server, exited.")
+        exit(1)
+        
 
-    asyncio.create_task(send_info())
-    asyncio.create_task(rcve_info())
+    asyncio.create_task(Socket.send_info())
+    asyncio.create_task(Socket.rcve_info())
     while True:
         Game.move_balls()
         Game.create_payload()
