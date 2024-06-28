e00bacc648b8883bb975fc2da9da458e4247a61f
caoslourenco <camillalourenco@outlook.com>
2024-06-28 11:11:38 -0300

    html: Notifications, Rankings and Statistics

--- frontend/app/src/pages/Friends.js
+++ frontend/app/src/pages/Friends.js
@@ -9,9 +9,9 @@ export default () => {
             @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
         @import url('https://fonts.googleapis.com/css2?family=Material+Icons+Round&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Play:wght@400;700&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap');
-        @import url('../css/Components.css');
-        @import url('../css/Colors.css');
-        @import url('../css/Typography.css');
+        @import url('../css/components.css');
+        @import url('../css/colors.css');
+        @import url('../css/typography.css');
         @import url('../css/pages/friends.css');
  
     `;
