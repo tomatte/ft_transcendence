673086226c4a9eba0d55196e2f5c831b5eda2080
caoslourenco <camillalourenco@outlook.com>
2024-06-29 02:17:28 -0300

    🔍 I started working on the matches and login. 🔍

--- frontend/app/src/pages/Matches.js
+++ frontend/app/src/pages/Matches.js
@@ -15,29 +15,106 @@ const Matches = () => {
 
 <div class="page-content__container__content">
     <div class="table__header-group">
-    <div class="table__header-group__data"><h6 class="table__header-group__data__title">PLAYER</h6></div>
-    <div class="table__header-group__data"> <h6 class="table__header-group__data__title">GAME TYPE</h6></div>
-    <div class="table__header-group__data"> <h6 class="table__header-group__data__title">SCORE</h6></div>
-    <div class="table__header-group__data"> <h6 class="table__header-group__data__title">STATUS</h6></div>
-    <div class="table__header-group__data"> <h6 class="table__header-group__data__title">DATE</h6></div>
+            <div class="table__header-group-data">
+            <span class="table__header">PLAYER</span>
+            </div>
+            <div class="table__header-group-data">
+            <span class="table__header">GAME TYPE</span>
+            </div>
+            <div class="table__header-group-data">
+            <span class="table__header">SCORE</span>
+            </div>
+            <div class="table__header-group-data">
+            <span class="table__header">DATE</span>
+            </div>
     </div>
-
+     
     <div class="table__row-group">
-    <div class="table__row-group__row"> 
-        <div class="table__row-group__row__data">        </div>
-        <div class="table__row-group__row__data">        </div>
-        <div class="table__row-group__row__data">        </div>
-        <div class="table__row-group__row__data">        </div>
-        <div class="table__row-group__row__data">        </div>
-        <div class="table__row-group__row__data">        </div>
-    </div>
-    <div class="table__row-group__row"> </div>
-    <div class="table__row-group__row"> </div>
-    <div class="table__row-group__row"> </div>
-    <div class="table__row-group__row"> </div>
-    </div>
-    <div class="nav-pagination">
+        <div class="table__row-group__row"> 
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+         </div>
+         <div class="table__row-group__row"> 
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+        </div>
+        <div class="table__row-group__row"> 
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+        </div>
+        <div class="table__row-group__row"> 
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+         </div>
+         <div class="table__row-group__row"> 
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+                <div class="table__row-group__row__data">        </div>
+         </div>
+         </div>
 
+    <div class="nav-pagination">
+        <div class="playground-container playground-container--pagination">
+            <nav class="pagination font-body-regular-bold">
+              <ul class="pagination__list">
+                <li class="pagination__control pagination__control--disabled">
+                  <a href="#">
+                    <span class="material-icons-round pagination__control__icon-left icon--medium">keyboard_double_arrow_left</span>
+                    <span class="pagination__control__text">First</span>
+                  </a>
+                </li>
+                <li class="pagination__control pagination__control--disabled">
+                  <a href="#">
+                    <span class="material-icons-round pagination__control__icon-left icon--medium">keyboard_double_arrow_left</span>
+                    <span class="pagination__control__text">Previous</span>
+                  </a>
+                </li>
+                <li class="pagination__item-number pagination__item-number--active"><a href="#">1</a></li>
+                <li class="pagination__item-number"><a href="#">2</a></li>
+                <li class="pagination__item-number"><a href="#">3</a></li>
+                <li class="pagination__item-number"><a href="#">4</a></li>
+                <li class="pagination__item-number"><a href="#">5</a></li>
+                <li class="pagination__item-number">
+                  <a href="#">
+                    <span class="material-icons-round icon--small">more_horiz</span>
+                  </a>
+                </li>
+                <li class="pagination__item-number"><a href="#">25</a></li>
+                <li class="pagination__control">
+                  <a class="pagination__control__link" href="#">
+                    <span class="material-icons-round pagination__control__icon-left icon--medium">keyboard_double_arrow_right</span>
+                    <span class="pagination__control__text">Next</span>
+                  </a>
+                </li>
+                <li class="pagination__control">
+                  <a class="pagination__control__link" href="#">
+                    <span class="material-icons-round pagination__control__icon-left icon--medium">keyboard_double_arrow_right</span>
+                    <span class="pagination__control__text">Last</span>
+                  </a>
+                </li>
+              </ul>
+          </nav>
+      
+          </div>
     </div>
 </div>
 
