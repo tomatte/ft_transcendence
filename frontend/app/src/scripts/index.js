0886e8a9158a1f2b66b10c043ef2fec177d15889
caoslourenco <camillalourenco@outlook.com>
2024-06-30 12:56:13 -0300

    NÃO FUNCIONA🎤

--- frontend/app/src/scripts/index.js
+++ frontend/app/src/scripts/index.js
@@ -1,38 +1,38 @@
-// import routes from './router.js';
+import routes from './router.js';
 
-// const container = document.querySelector('.page-content__container');
-// const sidebarMenuItems = document.querySelectorAll('.sidebar__menu-container .menu-item');
+const container = document.querySelector('.page-content__container');
+const sidebarMenuItems = document.querySelectorAll('.sidebar__menu-container .menu-item');
 
-// const renderPage = () => {
-//   const hash = window.location.hash.slice(1); // Get the hash excluding the '#'
-//   const page = hash || 'Home'; // Default to 'Home' if hash is empty
+const renderPage = () => {
+  const hash = window.location.hash.slice(1); // Get the hash excluding the '#'
+  const page = hash || 'Home'; // Default to 'Home' if hash is empty
 
-//   sidebarMenuItems.forEach(item => {
-//     item.classList.remove('menu-item--active');
-//   });
+  sidebarMenuItems.forEach(item => {
+    item.classList.remove('menu-item--active');
+  });
 
-//   if (routes[page]) {
-//     container.innerHTML = ''; // Clear previous content
-//     container.appendChild(routes[page]()); // Render the selected page component
+  if (routes[page]) {
+    container.innerHTML = ''; // Clear previous content
+    container.appendChild(routes[page]()); // Render the selected page component
     
-//     // Find the corresponding menu item and add 'menu-item--active' class
-//     const menuItem = document.querySelector(`.sidebar__menu-container .menu-item a[href="/#${page}"]`);
-//     if (menuItem) {
-//         menuItem.parentElement.classList.add('menu-item--active');
-//     }
-// } else {
-//     container.innerHTML = '<p>Página não encontrada</p>'; // Render a not found message
-// }
-// };
+    // Find the corresponding menu item and add 'menu-item--active' class
+    const menuItem = document.querySelector(`.sidebar__menu-container .menu-item a[href="/#${page}"]`);
+    if (menuItem) {
+        menuItem.parentElement.classList.add('menu-item--active');
+    }
+} else {
+    container.innerHTML = '<p>Página não encontrada</p>'; // Render a not found message
+}
+};
 
-// const init = () => {
-//   window.addEventListener('hashchange', renderPage); // Listen for hash changes
-// };
+const init = () => {
+  window.addEventListener('hashchange', renderPage); // Listen for hash changes
+};
 
-// window.addEventListener('load', () => {
-//   renderPage(); // Initial rendering based on current hash
-//   init(); // Initialize hashchange listener
-// });
+window.addEventListener('load', () => {
+  renderPage(); // Initial rendering based on current hash
+  init(); // Initialize hashchange listener
+});
 
 
 import routes from './router.js';
