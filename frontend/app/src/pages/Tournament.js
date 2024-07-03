92816acb57b182731ce9f9f63471a879f7643984
caoslourenco <camillalourenco@outlook.com>
2024-07-03 20:05:10 -0300

    🖖 We are all holding hands in UNPRODUCTIVITY 🤣

---
+++ frontend/app/src/pages/Tournament.js
@@ -0,0 +1,12 @@
+const Tournament = () => {
+	
+	const pageContentContainer = document.querySelector('.page-game__container');
+  
+	pageContentContainer.innerHTML = `
+  			 
+  `;
+
+  return pageContentContainer;
+}
+
+export default Tournament;
