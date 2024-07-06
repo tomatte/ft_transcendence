0327052e1f8be891a23a3448d397d94762bc62ad
Tomatte <daniel.brandao2000@gmail.com>
2024-07-06 17:58:23 -0300

    fix backend bug

--- backend/startDB.sh
+++ backend/startDB.sh
@@ -9,7 +9,5 @@ python manage.py migrate users
 python manage.py migrate --fake sessions zero
 python manage.py showmigrations
 python manage.py migrate --fake-initial
-python manage.py runserver
 
-
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:$BACKEND_PORT
