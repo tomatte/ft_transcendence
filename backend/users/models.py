694232461c0ebce107cda836148145d19e8570d0
Tomatte <daniel.brandao2000@gmail.com>
2024-07-09 16:53:47 -0300

    add default avatar to players

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
 	"""User model with additional fields."""
 	nickname = models.CharField(max_length=25)
-	avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+	avatar = models.ImageField(upload_to='images/avatars/', null=True, blank=True, default='../assets/images/players/avatars/default.webp')
 	friends = models.ManyToManyField('self', through='Friendship', symmetrical=False)
 	winners = models.IntegerField(default=0)
 	losses = models.IntegerField(default=0)
