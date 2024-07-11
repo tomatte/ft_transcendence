a4b2b86a55e080950c3c276298517756d2355e09
Tomatte <daniel.brandao2000@gmail.com>
2024-07-11 16:26:23 -0300

    remove AllowedOrigins

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -8,15 +8,13 @@ django_asgi_app = get_asgi_application()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
 
 application = ProtocolTypeRouter({
 	"http": django_asgi_app,
-	"websocket": AllowedHostsOriginValidator(
+	"websocket": 
 		AuthMiddlewareStack(
 			URLRouter([
 				*(game.urls.urlpatterns)
 			])
 		)
-	)
 })
