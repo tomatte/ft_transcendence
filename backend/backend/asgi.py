57f3637b69b3bb0fbb822321e1108a1277913493
Tomatte <daniel.brandao2000@gmail.com>
2024-07-11 15:24:28 -0300

    set Nginx as reverse proxy to django daphne

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,7 @@
 from django.core.asgi import get_asgi_application
 from django.urls import path
-import websocket
 import game.urls
 
-
-import websocket.views
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
