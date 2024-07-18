854d3a0d02ea92ae2ab00c3d1ff3797629498a88
caoslourenco <camillalourenco@outlook.com>
2024-07-18 15:03:34 -0300

    update empty pages 🔖

--- frontend/app/src/pages/Settings.js
+++ frontend/app/src/pages/Settings.js
@@ -8,8 +8,6 @@ const Settings = () => {
 
     pageContentContainer.innerHTML = `
  
-            <div class="page-content__container">
-
             <div class="page-content__container__header">
                 <div class="page-content__container__header__info">
                     <h4 class="page-content__container__header__info__title">Settings</h4>
@@ -17,26 +15,24 @@ const Settings = () => {
 
                 <button class="button button--primary">
                     <span class="button__text font-body-regular-bold">Save changes</span>
-                 </button>
-
+                </button>
             </div>
 
-            <div class="page-content__container__content">
+            <div class="settings_content">
 
                 <div class="page-content__container__content__setting">
 
                     <div class="page-content__container__content__setting__info">
-                        <span  class="page-content__container__content__setting__info__title font-body-medium-bold">Profile information</span>
-                        <span  class="page-content__container__content__setting__info__description font-body-medium">Customize your profile picture and change your name</span>
+                        <span class="page-content__container__content__setting__info__title font-body-medium-bold">Profile information</span>
+                        <span class="page-content__container__content__setting__info__description font-body-medium">Customize your profile picture and change your name</span>
                     </div>
                     <div class="page-content__container__content__setting__control-name">
                         <div class="profile-picture">
                             <img class="profile-picture__image" src="../../assets/images/players/tomatte.png" alt="Player Image"/>
  
-                                <button class="button button--outline">
-                                    <span class="button__text font-body-regular-bold">Update photo</span>
-                                </button>
- 
+                            <button class="button button--outline">
+                                <span class="button__text font-body-regular-bold">Update photo</span>
+                            </button>
                         </div>
                         <div class="input-name">
                             <div class="input">
@@ -45,44 +41,41 @@ const Settings = () => {
                                 <span class="input__helper-text font-body-caption-regular">It must contain only letters and numbers</span>
                             </div>
                         </div>
-
                     </div>
+                </div>      
 
                 <div class="divider"></div>
 
                 <div class="page-content__container__content__setting">
                     <div class="page-content__container__content__setting__info">
-                        <span  class="page-content__container__content__setting__info__title font-body-medium-bold">Controls</span>
-                        <span  class="page-content__container__content__setting__info__description font-body-medium">These settings are your navigation hub for seamless gameplay</span>
+                        <span class="page-content__container__content__setting__info__title font-body-medium-bold">Controls</span>
+                        <span class="page-content__container__content__setting__info__description font-body-medium">These settings are your navigation hub for seamless gameplay</span>
                     </div>
                     <div class="page-content__container__content__setting__control">
                         <div class="controls">
-                            <span  class="controls__type font-body-medium">Move up</span>
+                            <span class="controls__type font-body-medium">Move up</span>
                             <div class="controls__actions">
                                 <div class="keypad">
-                                    <span  class="search-bar__input">W</span>
+                                    <span class="keypad_control">W</span>
                                 </div>
                                 <div class="keypad">
-                                    <span class="material-icons-round button__icon-right">arrow_upward</span>
+                                    <span class="material-icons-round icon-large">arrow_upward</span>
                                 </div>
                             </div>
                         </div>
 
                         <div class="controls">
-                            <span  class="controls__type font-body-medium">Move down</span>
+                            <span class="controls__type font-body-medium">Move down</span>
 
                             <div class="controls__actions">
                                 <div class="keypad">
-                                    <span  class="search-bar__input">S</span>
+                                    <span class="keypad_control">S</span>
                                 </div>
                                 <div class="keypad">
-                                    <span class="material-icons-round button__icon-right">arrow_downward</span>
+                                    <span class="material-icons-round icon-large">arrow_downward</span>
                                 </div>
                             </div>
                         </div>
- 
-                        </div>
- 
  
                     </div>
                 </div>
@@ -91,35 +84,39 @@ const Settings = () => {
 
                 <div class="page-content__container__content__setting">
                     <div class="page-content__container__content__setting__info">
-                        <span  class="page-content__container__content__setting__info__title font-body-medium-bold">Sound</span>
-                        <span  class="page-content__container__content__setting__info__description font-body-medium">Toggle music and sound. Unlike space, here you can actually hear the action!</span>
+                        <span class="page-content__container__content__setting__info__title font-body-medium-bold">Sound</span>
+                        <span class="page-content__container__content__setting__info__description font-body-medium">Toggle music and sound. Unlike space, here you can actually hear the action!</span>
                     </div>
                     <div class="page-content__container__content__setting__control">
+
                         <div class="sound-toggle">
-                            <span  class="sound__type font-body-medium">Music</span>
-                            <div class="sound__actions">
-                                <div class="sound__actions__toggle">
-                                    <input type="checkbox" id="music" name="music" value="music">
-                                    <label for="music"></label>
-                                </div>
+                            <span class="sound__type font-body-medium">Music</span>
+                            <div class="toggle Active Enabled">
+                                <input type="checkbox" id="toggle-music" class="toggle-checkbox" checked>
+                                <label for="toggle-music" class="toggle-label">
+                                    <span class="toggle-inner"></span>
+                                    <span class="toggle-switch"></span>
+                                </label>
                             </div>
+
                         </div>
 
                         <div class="sound-toggle">
-                            <span  class="sound__type font-body-medium">Sound effects</span>
-                            <div class="sound__actions">
-                                <div class="sound__actions__toggle">
-                                    <input type="checkbox" id="sound-effects" name="sound-effects" value="sound-effects">
-                                    <label for="sound-effects"></label>
-                                </div>
+                            <span class="sound__type font-body-medium">Sound effects</span>
+                            <div class="toggle Active Enabled">
+                                <input type="checkbox" id="toggle-sound-effects" class="toggle-checkbox" checked>
+                                <label for="toggle-sound-effects" class="toggle-label">
+                                    <span class="toggle-inner"></span>
+                                    <span class="toggle-switch"></span>
+                                </label>
                             </div>
+
                         </div>
+
                     </div>
-                </div>
 
+                </div>
             </div>
-
-        </div>
 
   
     `;
