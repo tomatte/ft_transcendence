1a26bb032a552cecb4a1c79fd0567da32786406a
Tomatte <daniel.brandao2000@gmail.com>
2025-01-27 14:53:50 -0300

    new redirect url

--- frontend/app/src/js/FuncoesToInitialize.js
+++ frontend/app/src/js/FuncoesToInitialize.js
@@ -326,7 +326,7 @@ const PageLogin = () => {
 				</div>
 
 				<form class="login-form-button">
-					<a class="button button--primary" id="login-42-button" href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-a9bd2a36a6c5c5730287fe7c6aa4a7b0bcdee84025614824732a9730ee44625e&redirect_uri=https%3A%2F%2Flocalhost%2Fapi%2Fauth%2F&response_type=code">
+					<a class="button button--primary" id="login-42-button" href="https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-9954ea12a766e3c5e958dede25fc19868a2da863a3b70188bbf1d9c952d8433b&redirect_uri=https%3A%2F%2Flocalhost%2Fapi%2Fauth&response_type=code">
 						<img class="login-container__logo" src="/assets/logo/logo_42_white.svg" alt="logo logomark 42">
 						<span class="button__text font-body-regular-bold">Login with 42</span>
 					</a>
