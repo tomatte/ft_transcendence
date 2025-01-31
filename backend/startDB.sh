2de6a0b214bdbbfb1efa6f61965d1bfb5a3f16bd
Tomatte <daniel.brandao2000@gmail.com>
2025-01-31 11:08:56 -0300

    chore: remove unnecessary migration commands from startDb.sh

--- backend/startDB.sh
+++ backend/startDB.sh
@@ -3,14 +3,7 @@ set -e
 
 celery -A backend worker -D
 
-python manage.py makemigrations
-
-python manage.py migrate tournament
-python manage.py migrate users
-
-python manage.py migrate --fake sessions zero
-python manage.py showmigrations
-python manage.py migrate --fake-initial
+python manage.py migrate
 
 python manage.py runserver 0.0.0.0:$BACKEND_PORT
 
